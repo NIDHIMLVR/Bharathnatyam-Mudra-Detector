713c805cd6c400215d0ddfb7a8100b938ff8de69
Nidhi <nidhirameshmb@gmail.com>
2025-07-29 18:46:44 +0530

    app file fixes

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ def predict():
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "pong", 200
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use PORT env variable if present
     app.run(host='0.0.0.0', port=port)
